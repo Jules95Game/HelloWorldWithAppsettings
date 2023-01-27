a8bf4ea4b1b7621922a2f68913f2f826fa2006ad
Julian Romp <julian.romp@itvitaelearning.nl>
2023-01-27 10:52:17 +0100

    Add project files.

---
+++ HelloWorldWithAppsettings.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloWorldWithAppsettings.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
