7830c18bb27801a296d5fe7e2a6cbb920a32f4cc
Julian Romp <julian.romp@itvitaelearning.nl>
2023-01-27 12:04:43 +0100

    Get message from host

--- HelloWorldWithAppsettings.ConsoleApp/Program.cs
+++ HelloWorldWithAppsettings.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace HelloWorldWithAppsettings.ConsoleApp;
 
@@ -7,5 +9,8 @@ internal class Program
     private static void Main()
     {
         IHost host = Host.CreateDefaultBuilder().Build();
+        string message = host.Services.GetRequiredService<IConfiguration>().GetSection("ConsoleMessage").Value;
+        Console.WriteLine(message);
+        Console.ReadKey();
     }
 }
